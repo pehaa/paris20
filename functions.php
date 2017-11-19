24b892ce4c86cb8bd2550757c5f3a559a38ab575
Paulina Hetman <info@pehaa.com>
2017-11-19 15:08:59 +0100

    end part 2

--- functions.php
+++ functions.php
@@ -1,26 +1,73 @@
 <?php
-/*
- * On definie la taille d'image par défaut ...
+/**
+ * Paris20 fonctions et définitions
  */
-$GLOBALS['content_width'] = 720;
-/*
- * WordPress lui-même s'occupe de la balise title dans la head
- */
-add_theme_support( 'title-tag' );
 
-/*
- * On active les images à la une - par défaut c'est pas activé...
- */
-add_theme_support( 'post-thumbnails' );
+if ( ! function_exists( 'paris20_setup' ) ) :
 
-/*
- * On definie la taille d'image par défaut ...
- */
-set_post_thumbnail_size( 720, 0 );
+	function paris20_setup() {
+		/*
+		 * On dit à WordPress - c'est à toi de t'occuper 
+		 * de la balise title dans la head - on te fait confiance
+		 */
+		add_theme_support( 'title-tag' );
 
-/*
- * On definie la taille d'image par défaut ...
- */
-register_nav_menus( array(
-	'main'    => 'Menu Principal',
-) );
+		/*
+		 * On active les images à la une, par défaut pas activées
+		 *
+		 */
+		add_theme_support( 'post-thumbnails' );
+
+		/*
+		 * On definie la taille d'image par défaut
+		 * 720 x 0 veut dire 750px de largeur
+		 * et on imposer rien pour la hauter pour
+		 * garder les proportions de l'image
+		 */
+		set_post_thumbnail_size( 720, 0 );
+
+		/*
+		 * On enregistre ici un menu de navigation.
+		 * On l'appelle Menu dans le Header.
+		 * On a aussi besoin d'un identifiant (sans espaces)
+		 */
+		register_nav_menus( array(
+			'menu_header'    => 'Menu dans le Header',
+		) );
+
+		/*
+		 * On modernize le markup (html) de certaines parties
+		 * du code généré par WordPress
+		 */
+		add_theme_support( 'html5', array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+		) );
+
+		/*
+		 * On ajoute la possibilité d'ajouter le logo
+		 * via l'interface de WordPress !
+		 */
+		add_theme_support( 'custom-logo', array(
+			'height'      => 200,
+			'width'       => 200
+		) );
+	}
+endif;
+add_action( 'after_setup_theme', 'paris20_setup' );
+
+if ( ! function_exists( 'paris20_content_width' ) ) :
+
+	function paris20_content_width() {
+		/**
+		 * WordPress a besoin de cette info pour, par exemple, établir
+		 * la taille des vidéos qu'il va charger.
+		 * Dans notre cas tout le contenu est compris dans 720px donc :
+		 */
+		$GLOBALS['content_width'] = 720;
+	}
+endif;
+add_action( 'after_setup_theme', 'paris20_content_width', 0 );
--- index.php
+++ index.php
@@ -1,39 +1,43 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <?php wp_head(); ?>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<?php wp_head(); ?>
 </head>
 <body>
 <header id="masthead" class="site-header" role="banner">
-	<h1 class="site-title">Le titre de notre site</h1>
-	<p>Le slogan de notre site</p>
+	<?php if ( has_custom_logo() ) {
+		the_custom_logo();
+	} else { ?>
+		<h1 class="site-title"><?php echo get_bloginfo( 'name' ); ?></h1>
+		<p><?php echo get_bloginfo( 'description' ); ?></p>
+	<?php } ?>
 	<?php wp_nav_menu(); ?>
 </header>
 <main class="site-content" id="#content" role="main">
 <?php if (have_posts()) : ?>
-  <?php while (have_posts()) : the_post(); ?>
-    <article class="post" id="post-<?php the_ID(); ?>">
-      <h2>
-        <a href="<?php the_permalink(); ?>" rel="bookmark">
-          <?php the_title(); ?>
-        </a>
-      </h2>
-      <p class="meta">Publié le <?php the_time(); ?> par <?php the_author(); ?></p>
-      <?php the_post_thumbnail(); ?>
-      <?php the_content( 'Lire la suite...' ); ?>
-      <p>Publié dans <?php the_category(); ?></p>
-    </article>
-  <?php endwhile; ?>
-  <?php next_posts_link(); ?>
-  <?php previous_posts_link(); ?>
+	<?php while (have_posts()) : the_post(); ?>
+		<article class="post" id="post-<?php the_ID(); ?>">
+			<h2 class="post-title">
+				<a href="<?php the_permalink(); ?>" rel="bookmark">
+					<?php the_title(); ?>
+				</a>
+			</h2>
+			<p class="meta">Publié le <?php the_time(); ?> par <?php the_author(); ?></p>
+			<?php the_post_thumbnail(); ?>
+			<?php the_content( 'Lire la suite...' ); ?>
+			<p>Publié dans <?php the_category(); ?></p>
+		</article>
+	<?php endwhile; ?>
+	<?php next_posts_link(); ?>
+	<?php previous_posts_link(); ?>
 <?php else : ?>
-  <h2>Aucun résultat.</h2>
+	<h2>Aucun résultat.</h2>
 <?php endif; ?>
 </main>
-<footer>
-  Titre du site. © 2017
+<footer class="site-footer">
+	Titre du site. © 2017
 </footer>
 <?php wp_footer(); ?>
 </body>
